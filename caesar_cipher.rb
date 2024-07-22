ae3c53a8b46a14c3ab749f1c181128d23e89ad8b
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-07-22 11:29:19 +0100

    Remove unnecessary calculation and set it to a number literal

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,9 @@
 def caesar_cipher(string,shift)
   string.split("").map do |character|
     if character.between?("a","z")
-      ((character.ord + shift - "a".ord)%("z".ord - "a".ord + 1)+"a".ord).chr
+      ((character.ord + shift - "a".ord)%26+"a".ord).chr
     elsif character.between?("A","Z")
-      ((character.ord + shift - "A".ord)%("Z".ord - "A".ord + 1)+"A".ord).chr
+      ((character.ord + shift - "A".ord)%26+"A".ord).chr
     else
       character
     end
